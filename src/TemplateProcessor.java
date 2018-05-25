215c3d7013860dffd10d34a105a232b1df7f2e38
Steve <>
2018-05-25 15:17:15 +0100

    Extract literals as constants

--- src/TemplateProcessor.java
+++ src/TemplateProcessor.java
@@ -1,18 +1,21 @@
 public class TemplateProcessor {
 
+    public static final String CODE = "%CODE%";
+    public static final String ALTCODE = "%ALTCODE%";
+
     public String process(String template, String reqId, String code) {
 
         // Substitute for %CODE%
-        int templateSplitBegin = template.indexOf("%CODE%");
-        int templateSplitEnd = templateSplitBegin + 6;
+        int templateSplitBegin = template.indexOf(CODE);
+        int templateSplitEnd = templateSplitBegin + CODE.length();
         String templatePartOne = template.substring(0, templateSplitBegin);
         String templatePartTwo = template.substring(templateSplitEnd, template.length());
         code = reqId;
         template = templatePartOne + code + templatePartTwo;
 
         // Substitute for %ALTCODE%
-        templateSplitBegin = template.indexOf("%ALTCODE%");
-        templateSplitEnd = templateSplitBegin + 9;
+        templateSplitBegin = template.indexOf(ALTCODE);
+        templateSplitEnd = templateSplitBegin + ALTCODE.length();
         templatePartOne = template.substring(0, templateSplitBegin);
         templatePartTwo = template.substring(templateSplitEnd, template.length());
         String altcode = code.substring(0, 5) + "-" + code.substring(5, 8);
