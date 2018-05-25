bc79f88dea86a721faaeff1f62ec700b7017e8f0
Steve <>
2018-05-25 15:08:58 +0100

    Unnecesary strings removed

--- src/TemplateProcessor.java
+++ src/TemplateProcessor.java
@@ -2,24 +2,21 @@ public class TemplateProcessor {
 
     public String process(String sourceTemplate, String reqId, String code) {
 
-        String template = new String(sourceTemplate);
+        String template = sourceTemplate;
 
         // Substitute for %CODE%
         int templateSplitBegin = template.indexOf("%CODE%");
         int templateSplitEnd = templateSplitBegin + 6;
-        String templatePartOne = new String(template.substring(0,
-                templateSplitBegin));
-        String templatePartTwo = new String(template.substring(
-                templateSplitEnd, template.length()));
-        code = new String(reqId);
-        template = new String(templatePartOne + code + templatePartTwo);
+        String templatePartOne = template.substring(0, templateSplitBegin);
+        String templatePartTwo = template.substring(templateSplitEnd, template.length());
+        code = reqId;
+        template = templatePartOne + code + templatePartTwo;
 
         // Substitute for %ALTCODE%
         templateSplitBegin = template.indexOf("%ALTCODE%");
         templateSplitEnd = templateSplitBegin + 9;
-        templatePartOne = new String(template.substring(0, templateSplitBegin));
-        templatePartTwo = new String(template.substring(templateSplitEnd,
-                template.length()));
+        templatePartOne = template.substring(0, templateSplitBegin);
+        templatePartTwo = template.substring(templateSplitEnd, template.length());
         String altcode = code.substring(0, 5) + "-" + code.substring(5, 8);
         return templatePartOne + altcode + templatePartTwo;
     }
