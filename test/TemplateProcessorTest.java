9c9f35c5ea4abfcc2d57014be2dcb4352fb3cc06
Steve <>
2018-05-25 14:57:19 +0100

    Clean up tests

--- test/TemplateProcessorTest.java
+++ test/TemplateProcessorTest.java
@@ -4,27 +4,25 @@ import static org.junit.Assert.assertEquals;
 
 public class TemplateProcessorTest
 {
+    TemplateProcessor templateProcessor = new TemplateProcessor();
+
     @Test(expected = StringIndexOutOfBoundsException.class)
     public void shouldThrowAnStringIndexOutOfBoundsExceptionWhenNoCodeInTemplate() {
-        TemplateProcessor templateProcessor = new TemplateProcessor();
-        String result = templateProcessor.process("some stuff", "id", "code");
+        templateProcessor.process("some stuff", "id", "code");
     }
 
     @Test(expected = StringIndexOutOfBoundsException.class)
     public void shouldThrowAnStringIndexOutOfBoundsExceptionWhenNoAltcodeInTemplate() {
-        TemplateProcessor templateProcessor = new TemplateProcessor();
-        String result = templateProcessor.process("some stuff%CODE%", "id", "code");
+        templateProcessor.process("some stuff%CODE%", "id", "code");
     }
 
     @Test(expected = StringIndexOutOfBoundsException.class)
     public void shouldThrowAnStringIndexOutOfBoundsExceptionWhenReqIdIsShorterThan8() {
-        TemplateProcessor templateProcessor = new TemplateProcessor();
-        String result = templateProcessor.process("some stuff%CODE%some more stuff%ALTCODE%and some more", "1234567", "code");
+        templateProcessor.process("some stuff%CODE%some more stuff%ALTCODE%and some more", "1234567", "code");
     }
 
     @Test
     public void shouldReplaceCODEWithReqIdAndALTCODEWithReqIdWithADash() {
-        TemplateProcessor templateProcessor = new TemplateProcessor();
         String result = templateProcessor.process("some stuff%CODE%some more stuff%ALTCODE%and some more", "12345678", "code");
         assertEquals("some stuff12345678some more stuff12345-678and some more", result);
     }
