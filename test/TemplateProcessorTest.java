f292ebcf47e7d97984e344093c6baf6f373f30bb
Steve <>
2018-05-25 14:51:13 +0100

    Adding the first happy path test

--- test/TemplateProcessorTest.java
+++ test/TemplateProcessorTest.java
@@ -21,4 +21,11 @@ public class TemplateProcessorTest
         TemplateProcessor templateProcessor = new TemplateProcessor();
         String result = templateProcessor.process("some stuff%CODE%some more stuff%ALTCODE%and some more", "1234567", "code");
     }
+
+    @Test
+    public void shouldReplaceCODEWithReqIdAndALTCODEWithReqIdWithADash() {
+        TemplateProcessor templateProcessor = new TemplateProcessor();
+        String result = templateProcessor.process("some stuff%CODE%some more stuff%ALTCODE%and some more", "12345678", "code");
+        assertEquals("some stuff12345678some more stuff12345-678and some more", result);
+    }
 }
