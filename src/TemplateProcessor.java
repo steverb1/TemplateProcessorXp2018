ae1c997ebc801d18113fb54e9de28a92e5bf5ebd
Steve <>
2018-05-25 15:23:26 +0100

    Extract method, get rid of `code` parameter

--- src/TemplateProcessor.java
+++ src/TemplateProcessor.java
@@ -3,22 +3,24 @@ public class TemplateProcessor {
     public static final String CODE = "%CODE%";
     public static final String ALTCODE = "%ALTCODE%";
 
+    @Deprecated
     public String process(String template, String reqId, String code) {
+        return process(template, reqId);
+    }
 
-        // Substitute for %CODE%
+    public String process(String template, String reqId)
+    {
         int templateSplitBegin = template.indexOf(CODE);
         int templateSplitEnd = templateSplitBegin + CODE.length();
         String templatePartOne = template.substring(0, templateSplitBegin);
         String templatePartTwo = template.substring(templateSplitEnd, template.length());
-        code = reqId;
-        template = templatePartOne + code + templatePartTwo;
+        template = templatePartOne + reqId + templatePartTwo;
 
-        // Substitute for %ALTCODE%
         templateSplitBegin = template.indexOf(ALTCODE);
         templateSplitEnd = templateSplitBegin + ALTCODE.length();
         templatePartOne = template.substring(0, templateSplitBegin);
         templatePartTwo = template.substring(templateSplitEnd, template.length());
-        String altcode = code.substring(0, 5) + "-" + code.substring(5, 8);
+        String altcode = reqId.substring(0, 5) + "-" + reqId.substring(5, 8);
         return templatePartOne + altcode + templatePartTwo;
     }
 
