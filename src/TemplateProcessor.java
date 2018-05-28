6190b81258e7643e7994de04c0e24024a40ad736
Steve <>
2018-05-28 16:57:29 +0100

    last commit of the workshop

--- src/TemplateProcessor.java
+++ src/TemplateProcessor.java
@@ -10,18 +10,32 @@ public class TemplateProcessor {
 
     public String process(String template, String reqId)
     {
-        int templateSplitBegin = template.indexOf(CODE);
-        int templateSplitEnd = templateSplitBegin + CODE.length();
+        String variableName = CODE;
+        template = replace(template, reqId, variableName);
+        int templateSplitBegin;
+        int templateSplitEnd;
+        String templatePartOne;
+        String templatePartTwo;
+
+        String altcode = reqId.substring(0, 5) + "-" + reqId.substring(5, 8);
+
+        variableName = ALTCODE;
+        templateSplitBegin = template.indexOf(variableName);
+        templateSplitEnd = templateSplitBegin + variableName.length();
+        templatePartOne = template.substring(0, templateSplitBegin);
+        templatePartTwo = template.substring(templateSplitEnd, template.length());
+        String result = templatePartOne + altcode + templatePartTwo;
+        return result;
+    }
+
+    private String replace(String template, String reqId, String variableName)
+    {
+        int templateSplitBegin = template.indexOf(variableName);
+        int templateSplitEnd = templateSplitBegin + variableName.length();
         String templatePartOne = template.substring(0, templateSplitBegin);
         String templatePartTwo = template.substring(templateSplitEnd, template.length());
         template = templatePartOne + reqId + templatePartTwo;
-
-        templateSplitBegin = template.indexOf(ALTCODE);
-        templateSplitEnd = templateSplitBegin + ALTCODE.length();
-        templatePartOne = template.substring(0, templateSplitBegin);
-        templatePartTwo = template.substring(templateSplitEnd, template.length());
-        String altcode = reqId.substring(0, 5) + "-" + reqId.substring(5, 8);
-        return templatePartOne + altcode + templatePartTwo;
+        return template;
     }
 
 }
