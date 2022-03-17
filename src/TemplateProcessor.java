040218a5fa975584cabee242c3ecc6e5b89c8276
steverb1 <steverb@mindspring.com>
2022-03-17 13:58:22 -0600

    Inline replace method.  More cleanup.

--- src/TemplateProcessor.java
+++ src/TemplateProcessor.java
@@ -8,19 +8,10 @@ public class TemplateProcessor {
         return process(template, reqId);
     }
 
-    public String process(String template, String reqId)
-    {
-        String variableName = CODE;
-        template = replace(template, reqId, variableName);
-        String altcode = reqId.substring(0, 5) + "-" + reqId.substring(5, 8);
+    public String process(String template, String replacement) {
+        template = template.replace(CODE, replacement);
+        String hyphenatedReplacement = replacement.substring(0, 5) + "-" + replacement.substring(5, 8);
 
-        variableName = ALTCODE;
-        String result = replace(template, altcode, variableName);
-        return result;
-    }
-
-    private String replace(String template, String reqId, String variableName)
-    {
-        return template.replace(variableName, reqId);
+        return template.replace(ALTCODE, hyphenatedReplacement);
     }
 }
